baa9201e879f6bc72d89716e12b5de45d295b916
jonatan <jpizarroserrano@iesesteveterradas.cat>
2017-10-11 19:31:13 +0200

    2 -

--- yolo.php
+++ yolo.php
@@ -74,28 +74,44 @@ table {
 	<?php  
 		$columnas= $_POST["colum"];
 		$filas = $_POST["fila"];
-		$a= "<img  src='imagenes/asd.jpeg' width='90' height='100'>";
+
+		$carta = "<img  src='imagenes/carta.jpeg' width='90' height='100'>";
+
+		$AsDiam= "<img  src='imagenes/asd.jpeg' width='90' height='100'>";
+		$AsCor= "<img  src='imagenes/asc.png' width='90' height='100'>";
+		$AsPic= "<img  src='imagenes/aspicas.jpeg' width='90' height='100'>";
+		$AsTre= "<img  src='imagenes/ast.png' width='90' height='100'>";
+		$Jcor= "<img  src='imagenes/jcor.jpeg' width='90' height='100'>";
+		$cor= "<img  src='imagenes/10cor.jpeg' width='90' height='100'>";
+
+		$cartas = array($AsDiam,$AsCor,$AsPic,$AsTre,$Jcor );
+		shuffle($cartas);
+		echo "$cartas";
+
 		
-		echo "$a";
+		
 		for($f=0;$f<$columnas;$f++){
 				
 					echo "<tr>";
 
 					for ($i=0; $i <$filas ; $i++) {
-				
-							echo "<td >
-							<label>
-								<input type='checkbox'  />
-							    <div class='card'>
-							         <div class='front'><img  src='imagenes/carta.jpeg' width='90' height='100'></div>
-					        		<div class='back'>$a</div>
-							    </div>
-							</label>
+
+							foreach ($cartas as $l ) {
+								echo "<td >
+								<label>
+									<input type='checkbox'  />
+								    <div class='card'>
+								         <div class='front'><img  src='imagenes/carta.jpeg' width='90' height='100'></div>
+						        		 <div class='back'>$l</div>
+								    </div>
+								</label>
 
 
-							 </td>";
-																		
-					}
+								 </td>";
+							}
+							
+																			
+						}
 
 					echo "</tr>";
 				}
