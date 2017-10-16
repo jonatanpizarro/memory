66318a9c95539446b2306bbd1f2f87d987fd269a
jonatan <jpizarroserrano@iesesteveterradas.cat>
2017-10-16 18:22:19 +0200

    Las cartas salen random y repitiendose 2 veces

--- yolo.php
+++ yolo.php
@@ -75,40 +75,69 @@ table {
 		$columnas= $_POST["colum"];
 		$filas = $_POST["fila"];
 
+		$total=($filas*$columnas)/2;
+
 		$carta = "<img  src='imagenes/carta.jpeg' width='90' height='100'>";
 
 		$AsDiam= "<img  src='imagenes/asd.jpeg' width='90' height='100'>";
 		$AsCor= "<img  src='imagenes/asc.png' width='90' height='100'>";
 		$AsPic= "<img  src='imagenes/aspicas.jpeg' width='90' height='100'>";
 		$AsTre= "<img  src='imagenes/ast.png' width='90' height='100'>";
-		$Jcor= "<img  src='imagenes/jcor.jpeg' width='90' height='100'>";
+		$KCor="<img  src='imagenes/kcor.png' width='90' height='100'>";
+		$KPic="<img  src='imagenes/kpic.jpg' width='90' height='100'>";
+		$KDia="<img  src='imagenes/kdia.jpeg' width='90' height='100'>";
+		$KTre="<img  src='imagenes/ktre.jpg' width='90' height='100'>";
+		$JCor= "<img  src='imagenes/jcor.png' width='90' height='100'>";
+		$JDia= "<img  src='imagenes/jdia.jpeg' width='90' height='100'>";
+		$JTrev= "<img  src='imagenes/jtrev.jpeg' width='90' height='100'>";
+		$JPic= "<img  src='imagenes/jpic.jpeg' width='90' height='100'>";
 		$cor= "<img  src='imagenes/10cor.jpeg' width='90' height='100'>";
 
-		$cartas = array($AsDiam,$AsCor,$AsPic,$AsTre,$Jcor );
-		shuffle($cartas);
-		echo "$cartas";
-
+		$cartas = array($AsDiam,$AsCor,$AsPic,$AsTre,$JCor,$KCor,$KPic,$KDia,$KTre,$JCor,$JDia,$JTrev,$JPic );
 		
+
+		shuffle($cartas);
+		$mezcla=array();
+
+		$contador=-1;
+
+		for ($i=0; $i <$total ; $i++) {
+						$l= $cartas[$i];
+						array_push($mezcla,$l);
+						array_push($mezcla,$l);
+						shuffle($mezcla);
+					}
+
+
+		$long=count($mezcla);
+		//echo "$long";
+				
 		
 		for($f=0;$f<$columnas;$f++){
 				
 					echo "<tr>";
+					
 
 					for ($i=0; $i <$filas ; $i++) {
-
-							foreach ($cartas as $l ) {
+						//echo "$i";
+					
+						$contador++;
+							//echo "$contador";
+							$a=$mezcla[$contador];
+													
 								echo "<td >
 								<label>
 									<input type='checkbox'  />
 								    <div class='card'>
 								         <div class='front'><img  src='imagenes/carta.jpeg' width='90' height='100'></div>
-						        		 <div class='back'>$l</div>
+						        		 <div class='back'>$a</div>
 								    </div>
 								</label>
 
 
 								 </td>";
-							}
+						
+										
 							
 																			
 						}
