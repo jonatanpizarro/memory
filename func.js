744b96709d7f731facb88baf856284e0266f7d38
jonatan <jpizarroserrano@iesesteveterradas.cat>
2017-10-25 17:19:15 +0200

    Funciones js y cambio de asignacion id a las cartas

--- func.js
+++ func.js
@@ -1,45 +1,92 @@
 var clics2=0;
 var clicks=0;
-var a;
-var b;
-var target;
+var carta1;
+var carta2;
+var label1;
+var puntuacion =0;
+var intentos =0;
+var bloqueo;
+var cartas=0;
 
-/*function start(cont){
-	    comprobar(cont);
+/*function start(pos,id){
+		bloquear(pos);
+	    comprobar(pos,id);
 
 }*/
 
-//function front()
-
-function comprobar(cont){
-	clics2++;
 
 
-	if (clics2==1) {
-		a=document.getElementsByClassName('card')[cont].childNodes[1].parentNode.childNodes[3].childNodes[1].src;
-		alert("se ha cambiado a");
-		alert(a);
-	}else{
-		b=document.getElementsByClassName('card')[cont].childNodes[1].parentNode.childNodes[3].childNodes[1].src;
-		alert("se ha cambiado b");
-		clics2=0;
-		alert(b);
-	}	
+function bloquear(pos){
+	document.getElementById("check"+pos).disabled=true;
 
-	if (clics2==0) {
-		if (a==b) {
-			alert("igual");
-			
-		}else{
-			alert("nop");
-			
-			
-		}}
-	
+}
 
+function desbloquear(pos,pos1){
+	document.getElementById("check"+pos).disabled=false;
+	document.getElementById("check"+pos1).disabled=false;
+}
 
-	
-	
+function darVuelta(a,b){
+	a.click();
+	b.click();
+}
+
+function intentos1(intentos){
+	document.getElementById('intentos').innerHTML="Intentos :"+intentos;
+}
+
+function bloquearTodo(pos){
+	for (var i =0; i<=cartas ; i++) {
+		document.getElementById("check"+i).disabled=true;
+	}
+}
+
+function borraId(pos1,pos2){
+	document.getElementsById(pos1).removeAttribute("id");
+	document.getElementsById(pos2).removeAttribute("id");
+
+}
+
+function cogerValor(total){
+	cartas=total*2;
 }
 
 
+
+function comprobar(pos,id){
+	clics2++;
+	intentos1(intentos);
+
+
+	if (clics2==1) {
+		carta1=id;
+		pos1=pos;
+		label1=document.getElementsByClassName('card')[pos].parentNode;
+	}
+	
+	else if (clics2==2){
+		bloquearTodo();
+		pos2=pos;
+		carta2=id;
+		label2=document.getElementsByClassName('card')[pos].parentNode;
+		
+
+		if (carta1==carta2) {
+			puntuacion++;	
+			intentos++;
+			borraId(pos1,pos2);
+			intentos1(intentos);		
+			
+		}else{
+			
+			setTimeout( function(){
+				clics2=0;				
+				desbloquear(pos1,pos2);
+				darVuelta(label1,label2)
+			} , 2000);
+			intentos++;
+			intentos1(intentos);
+			
+		}
+	}	
+}
--- yolo.php
+++ yolo.php
@@ -3,61 +3,74 @@
 <script src="func.js" type="text/javascript"></script>
 
 </head>
-<div>
-	<div id="info" >
-
-	</div>
-
-		<table >
+<body >
+	<div>
+		<div id="info" >
+		<p id="intentos"> </p>
 
 
+		</div>
 
-		<?php  
-			$columnas= $_POST["colum"];
-			$filas = $_POST["fila"];
-
-			$parejas=($filas*$columnas)/2; //Esto te da el numero de cartas que vas a necesitar
-
-			$cartas = array();
-
-			for ($i=1; $i <=$parejas; $i++) { 
-				$car="<img id=$i  src='imagenes/$i.png' width='90' height='100'>";
-				array_push($cartas,$car);
-				array_push($cartas,$car);
-			}
-			shuffle($cartas);
-
-			$contador=-1;
+			<table >
 
 
-		
-			for($f=0;$f<$columnas;$f++){
-					
-						echo "<tr>";
 
+			<?php  
+				$columnas= $_POST["colum"];
+				$filas = $_POST["fila"];
+
+				$parejas=($filas*$columnas)/2; //Esto te da el numero de cartas que vas a necesitar
+
+				$cartas = array();
+
+				$contador=0;
+				
+
+
+				for ($i=1; $i <=$parejas; $i++) { 
+					$car=$i;
+					array_push($cartas,$car);
+					array_push($cartas,$car);
+				}
+				shuffle($cartas);
+
+				$posicion=0;
+
+
+			
+				for($f=0;$f<$columnas;$f++){
 						
+							echo "<tr>";
 
-						for ($i=0; $i <$filas ; $i++) {
 							
-																
-								$contador++;
-									//echo "$contador";
-								$a=$cartas[$contador];
-														
-								echo "<td>
-								<label>
-									<input type='checkbox' />
-									    <div class='card' id='$contador' onClick='comprobar(".$contador.")' >
-									         <div class='front'  ><img   src='imagenes/carta.jpeg' width='90' height='100'></div>
-							        		 <div class='back'> $a </div>
-									    </div>
-									</label>
-									 </td>";																				
+
+							for ($i=0; $i <$filas ; $i++) {
+								
+																	
+									
+										//echo "$contador";
+									$a=$cartas[$contador];
+															
+									echo "<td>
+									<label>
+										<input class='che' id='check$posicion' type='checkbox'/>
+										    <div class='card'>
+
+										         <div class='front'  ><img   onClick='comprobar(".$posicion.",".$a.")' src='imagenes/carta.jpeg' width='90' height='100'></div>
+
+								        		 <div class='back'><img id=".$a." onClick='bloquear(".$posicion.",".$a.")' src='imagenes/".$a.".png' width='90' height='100'></div>
+										    </div>
+										</label>
+										 </td>";
+										 $contador++;	
+										 $posicion++;																			
+							}
+							
+
+							echo "</tr>";
 						}
+				?>
 
-						echo "</tr>";
-					}
-			?>
-
-		</table>
-</div>
+			</table>
+	</div>
+</body onload="cogerValor($parejas)">
